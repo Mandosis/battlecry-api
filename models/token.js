29b4f5567efc4d552ca68bd8bf480995877c3580
Chris Rabuse <rab233@live.com>
2016-05-04 18:26:26 -0500

    Remove encryption from tokens

--- models/token.js
+++ models/token.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt');
 
 var Schema = mongoose.Schema;
-var SALT_WORK_FACTOR = 10;
 
 var tokenSchema = new Schema({
   token: {type: String, required: true, unique: true},
@@ -10,45 +8,6 @@ var tokenSchema = new Schema({
   owner: {type: String, required: true},
   use: {type: String, required: true}
 });
-
-// Encrypt token before saving to the database
-tokenSchema.pre('save', function(next) {
-  var token = this;
-
-  // If the token was not changed, do not continue
-  if (!user.isModified('token')) {
-    return next();
-  }
-
-  // Create salt for hash
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) {
-      return next(err);
-    }
-
-    bcrypt.hash(token.token, salt, function(err, hash) {
-      if (err) {
-        return next(err);
-      }
-
-      // Replace clear text with hashed token
-      token.token = hash;
-
-      next();
-    });
-  });
-});
-
-// Compare the proided token against the database
-tokenSchema.methods.compareToken = function(candidateToken, done) {
-  bcrypt.compare(candidateToken, this.password, function(err, isMatch) {
-    if (err) {
-      return done(err);
-    } else {
-      done(null, isMatch);
-    }
-  });
-};
 
 var Token = mongoose.model('token', tokenSchema)
 
